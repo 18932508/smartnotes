19ffdc497d89426f400088e99e92180c5a128d36
18932508 <18932508@users.noreply.github.com>
2018-08-27 21:41:53 +1000

    part of make meeting
    why

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -6,19 +6,93 @@ import MakeMeeting from'./MakeMeeting';
 import NavbarMenu from './Navbar';
 import UpcomingMeetings from './UpcomingMeetings';
 import PastMeetings from './PastMeetings';
+import Popup from "reactjs-popup";
 
 export default class MainMenu extends Component{
+constructor(props)
+    {
+        super(props);
+        this.state ={
+            vdate : '',
+            vroom : '',
+            vtime : '',
+            vtype : '',
+
+            newMeeting : {
+                code : 0,
+                date : '',
+                room : '',
+                time: '',
+                type: ''
+            }
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleSubmit(event) {
+        let upcomingMeetingList = this.props.serverData.user.upcomingMeetings;
+        let newMeetingCode = upcomingMeetingList[upcomingMeetingList.length - 1].code + 1
+
+        console.log(upcomingMeetingList)
+        let newMeeting =  Object.assign([], this.state.newMeeting);
+
+        newMeeting.code = this.state.newMeetingCode
+        newMeeting.date= this.state.vdate
+        newMeeting.room = this.state.vroom
+        newMeeting.time= this.state.vtime
+        newMeeting.type=  this.state.vtype
+        
+        console.log(this.props.newMeeting)
+
+        let serverData =  Object.assign([],this.state.serverData);
+        console.log(serverData)
+        this.setState(this.state.serverData.user.upcomingMeetings[this.state.upcomingMeetingList.length] = (this.state.newMeeting));
+        console.log('REEEE')
+        console.log(this.state.serverData.user.upcomingMeetings)
+
+        event.preventDefault();
+    }
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+      }
 
 render()
 {
     let serverData = this.props.serverData
-    console.log(serverData)
     return(            
             <div>
             <NavbarMenu />
             <h1>Welcome {serverData.user.name}</h1>
             <MeetingMode />
-            <MakeMeeting />
+            
+
+            <div style={{display: 'inline-block'}}>
+                <Popup
+                trigger={<button className="meetingModeButton" onClick={() => this.handleClick}>Make Meeting</button>}
+                modal
+                closeOnDocumentClick>
+                    <div className="newNote">
+                        <header style={{background: "#F7941D" }}> New Upcoming Meeting</header>
+                        <form onSubmit={this.handleSubmit}>
+                        <label> Date : 
+                        <input type="text" name="date" className="inputBox" value={this.state.vdate} onChange={this.handleChange}/>
+                        </label>
+                        <label> Room : 
+                        <input type="text" name="room" className="inputBox" value={this.state.vroom} onChange={this.handleChange}/>
+                        </label>
+                        <label> Time : 
+                        <input type="text" name="time" className="inputBox" value={this.state.vtime} onChange={this.handleChange}/>
+                        </label>
+                        <label> Type : 
+                        <input type="text" name="type" className="inputBox" value={this.state.vtype} onChange={this.handleChange}/>
+                        </label>
+                        <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                </Popup>    
+            </div>
+
+
             <div className="why">
             <UpcomingMeetings serverData={serverData.user.upcomingMeetings}/>
             <PastMeetings serverData={serverData.user.pastMeetings}/>
--- src/MakeMeeting.js
+++ src/MakeMeeting.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
+import Popup from "reactjs-popup";
 
 export default class MakeMeeting extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state ={
+        date : '',
+        room : '',
+        time : '',
+        type : ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleSubmit(event) {
+
+        event.preventDefault();
+    }
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+      }
+
 render()
 {
+    let upcomingMeetingList = this.props.serverData
     return(
+        
         <div style={{display: 'inline-block'}}>
-            <button className="meetingModeButton">Make Meeting</button>
+        {console.log(upcomingMeetingList )}
+                <Popup
+        trigger={<button className="meetingModeButton" onClick={() => this.handleClick}>Make Meeting</button>}
+        modal
+        closeOnDocumentClick>
+        <div className="newNote">
+        <header style={{background: "#F7941D" }}> New Upcoming Meeting</header>
+        <form onSubmit={this.handleSubmit}>
+        <label> Date : 
+        <input type="text" name="date" className="inputBox" date={this.state.date} onChange={this.handleChange}/>
+        </label>
+        <label> Room : 
+        <input type="text" name="room" className="inputBox" room={this.state.room} onChange={this.handleChange}/>
+        </label>
+        <label> Time : 
+        <input type="text" name="time" className="inputBox" time={this.state.time} onChange={this.handleChange}/>
+        </label>
+        <label> Type : 
+        <input type="text" name="type" className="inputBox" type={this.state.type} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+        </div>
+        </Popup>    
         </div>
     )
 }
